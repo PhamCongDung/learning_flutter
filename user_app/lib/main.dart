f6ebf1617f427f9ed9ac4ea22366d94f0d8d2247
DungPham <phamcongdung92@gmail.com>
2026-01-08 20:49:34 +0700

    Flutter day 4: Future UI

--- user_app/lib/main.dart
+++ user_app/lib/main.dart
@@ -11,148 +11,207 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('User App')),
-        body: const CounterPage(),
+        body: const ProfilePage(),
       ),
     );
   }
 }
 
-class CounterPage extends StatefulWidget {
-  const CounterPage({super.key});
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
   @override
-  State<CounterPage> createState() => _CounterPageState();
+  State<ProfilePage> createState() => _ProfilePage();
 }
 
-class _CounterPageState extends State<CounterPage> {
-  int count = 0;
-  bool get isHigh => count > 5;
+class _ProfilePage extends State<ProfilePage> {
+  String? userName;
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('ProfilePage initState');
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    setState(() {
+      isLoading = true;
+    });
+    final name = await fetchUserName();
+    setState(() {
+      userName = name;
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
+    debugPrint('ProfilePage build');
+    if (isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Count : $count',
-            style: TextStyle(
-              fontSize: isHigh ? 48 : 32,
-              fontWeight: isHigh ? FontWeight.bold : FontWeight.normal,
-              color: isHigh ? Colors.red : Colors.black,
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                count++;
-              });
-            },
-            child: const Text('+1'),
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                count == 0 ? count = 0 : count--;
-              });
-            },
-            child: const Text('-1'),
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                count = 0;
-              });
-            },
-            child: const Text('Reset'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final shouldReset = await Navigator.push<bool>(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      DetailPage(count: count, isHigh: isHigh),
-                ),
-              );
-              if (shouldReset == true) {
-                setState(() {
-                  count = 0;
-                });
-              }
-            },
-            child: const Text('Go to Detail'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SettingPage(count: count),
-                ),
-              );
-            },
-            child: const Text('SettingPage'),
-          ),
+          Text('Hello $userName', style: TextStyle(fontSize: 24)),
+          SizedBox(height: 20),
+          ElevatedButton(onPressed: loadData, child: const Text('Reload')),
         ],
       ),
     );
   }
-}
 
-class DetailPage extends StatelessWidget {
-  final int count;
-  final bool isHigh;
-  const DetailPage({super.key, required this.count, required this.isHigh});
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: const Text('Detail Page')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              isHigh ? 'Count is High : $count' : 'Count is NORMAL : $count',
-              style: const TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context, true);
-              },
-              child: const Text('Reset Count'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context, false);
-              },
-              child: const Text('Back'),
-            ),
-          ],
-        ),
-      ),
-    );
+  void dispose() {
+    debugPrint('ProfilePage dispose');
+    super.dispose();
   }
 }
 
-class SettingPage extends StatelessWidget {
-  final int count;
-  const SettingPage({super.key, required this.count});
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: Text('Setting Page')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Count is : $count', style: const TextStyle(fontSize: 24)),
-          ],
-        ),
-      ),
-    );
-  }
+Future<String> fetchUserName() async {
+  await Future.delayed(const Duration(seconds: 10));
+  return 'Dung';
 }
+
+//class CounterPage extends StatefulWidget {
+//  const CounterPage({super.key});
+//  @override
+//  State<CounterPage> createState() => _CounterPageState();
+//}
+//
+//class _CounterPageState extends State<CounterPage> {
+//  int count = 0;
+//  bool get isHigh => count > 5;
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Column(
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: [
+//          Text(
+//            'Count : $count',
+//            style: TextStyle(
+//              fontSize: isHigh ? 48 : 32,
+//              fontWeight: isHigh ? FontWeight.bold : FontWeight.normal,
+//              color: isHigh ? Colors.red : Colors.black,
+//            ),
+//          ),
+//          const SizedBox(height: 20),
+//          ElevatedButton(
+//            onPressed: () {
+//              setState(() {
+//                count++;
+//              });
+//            },
+//            child: const Text('+1'),
+//          ),
+//          const SizedBox(height: 10),
+//          ElevatedButton(
+//            onPressed: () {
+//              setState(() {
+//                count == 0 ? count = 0 : count--;
+//              });
+//            },
+//            child: const Text('-1'),
+//          ),
+//          const SizedBox(height: 10),
+//          ElevatedButton(
+//            onPressed: () {
+//              setState(() {
+//                count = 0;
+//              });
+//            },
+//            child: const Text('Reset'),
+//          ),
+//          ElevatedButton(
+//            onPressed: () async {
+//              final shouldReset = await Navigator.push<bool>(
+//                context,
+//                MaterialPageRoute(
+//                  builder: (context) =>
+//                      DetailPage(count: count, isHigh: isHigh),
+//                ),
+//              );
+//              if (shouldReset == true) {
+//                setState(() {
+//                  count = 0;
+//                });
+//              }
+//            },
+//            child: const Text('Go to Detail'),
+//          ),
+//          ElevatedButton(
+//            onPressed: () {
+//              Navigator.push(
+//                context,
+//                MaterialPageRoute(
+//                  builder: (context) => SettingPage(count: count),
+//                ),
+//              );
+//            },
+//            child: const Text('SettingPage'),
+//          ),
+//        ],
+//      ),
+//    );
+//  }
+//}
+//
+//class DetailPage extends StatelessWidget {
+//  final int count;
+//  final bool isHigh;
+//  const DetailPage({super.key, required this.count, required this.isHigh});
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Scaffold(
+//      appBar: AppBar(title: const Text('Detail Page')),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: [
+//            Text(
+//              isHigh ? 'Count is High : $count' : 'Count is NORMAL : $count',
+//              style: const TextStyle(fontSize: 24),
+//            ),
+//            ElevatedButton(
+//              onPressed: () {
+//                Navigator.pop(context, true);
+//              },
+//              child: const Text('Reset Count'),
+//            ),
+//            ElevatedButton(
+//              onPressed: () {
+//                Navigator.pop(context, false);
+//              },
+//              child: const Text('Back'),
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
+//class SettingPage extends StatelessWidget {
+//  final int count;
+//  const SettingPage({super.key, required this.count});
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Scaffold(
+//      appBar: AppBar(title: Text('Setting Page')),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: [
+//            Text('Count is : $count', style: const TextStyle(fontSize: 24)),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
